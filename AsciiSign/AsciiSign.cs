c98bcc49f4e1112e056234c0b4bd84da4bf1a487
ericssendelima <ericssendelima@gmail.com>
2025-11-09 09:39:42 -0300

    docs: updating some examples images

--- AsciiSign/AsciiSign.cs
+++ AsciiSign/AsciiSign.cs
@@ -1,4 +1,26 @@
-﻿namespace AsciiSign;
+﻿/*
+ * AsciiSign
+ * Copyright (C) 2025 Yalê Ericssen de Lima
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Lesser General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public License
+ * along with this program.  If not, see <https://www.gnu.org/licenses/>.
+ */
+
+using AsciiSign.utils.characterDictionaries;
+using AsciiSign.utils.enums;
+using AsciiSign.utils.services;
+
+namespace AsciiSign;
 
 /// <summary>
 /// Static class to render text in ASCII art style in the console.
@@ -9,132 +31,27 @@
 /// </remarks>
 public static class Sign
 {
-  /// <summary>
-  /// Renders the given text in ASCII art style in the console.
-  /// </summary>
-  /// <param name="text">The text to render.</param>
-  /// <exception cref="ArgumentException">Thrown when the text is null, empty, or exceeds 12 characters.</exception>
-  /// <remarks>
-  /// Each character is represented in a 5x5 grid using binary signatures.
-  /// The rendered text is framed with a border for better visibility.
-  /// </remarks>
-  /// <example>
-  /// Sign.DrawString("HELLO");
-  /// </example>
-  public static void DrawString(string text, bool isInvertedSign)
+  public static void DrawTuringFont(string text)
+  {
+    DrawString<string>(text, FontType.TURING, Turing.Instance, MatrixHeight.SIX);
+  }
+
+  public static void DrawShadedFont(string text)
+  {
+    DrawString<string>(text, FontType.SHADED, Shaded.Instance, MatrixHeight.SIX);
+  }
+
+  public static void DrawBasicFont(string text, bool isInvertedSign)
+  {
+    DrawString<int>(text, FontType.BASIC, Basic.Instance, MatrixHeight.FIVE, isInvertedSign);
+  }
+  private static void DrawString<T>(string text, FontType fontType, CharacterDictionary<T> characterMap, MatrixHeight matrixHeight, bool? isInvertedSign = false)
   {
     // Get the array of characters from the input text
-    char[] letters = GetCharactersArray(text);
+    char[] letters = DataProcessing<T>.GetCharactersArray(text, fontType);
 
-    // Get the signatures for each character in the text
-    Dictionary<char, int[]> textSignatures = GetTextSignatures(letters);
+    T[,] textMatrixSignatures = DataProcessing<T>.GetTextMatrixSignatures(text, fontType, characterMap, matrixHeight);
 
-    // Create a matrix to hold the signatures for rendering
-    int[,] textMatrixSignatures = GetTextMatrixSignatures(letters, textSignatures);
-
-    // Render the text in the console
-    Console.WriteLine();
-    Console.Write("██");
-    Console.WriteLine(new string('█', letters.Length * 10 + (letters.Length - 1) * 2 + 6));
-    Console.WriteLine("██" + new string(isInvertedSign ? '█' : ' ', letters.Length * 10 + (letters.Length - 1) * 2 + 4) + "██");
-    // Render each of the 5 lines
-    for (int line = 0; line < 5; line++)
-    {
-      Console.Write(isInvertedSign ? "████" : "██  ");
-      // Render each character in the line
-      for (int decimalElement = 0; decimalElement < letters.Length; decimalElement++)
-      {
-        // Get the decimal value for the current character line and convert it to binary string (padded to 5 bits) 
-        int dec = textMatrixSignatures[line, decimalElement];
-        string binary = Convert.ToString(dec, 2).PadLeft(5, '0');
-
-        // Render each bit in the binary string
-        foreach (char bit in binary)
-        {
-          // Render a filled pixel for '1' and an empty pixel for '0'
-          if (bit == '1')
-          {
-            Console.Write(isInvertedSign ? "  " : "██");
-          }
-          else
-          {
-            Console.Write(isInvertedSign ? "██" : "  ");
-          }
-        }
-        Console.Write(isInvertedSign ? "██" : "  ");
-      }
-      Console.Write("██");
-      Console.WriteLine();
-    }
-    Console.WriteLine("██" + new string(isInvertedSign ? '█' : ' ', letters.Length * 10 + (letters.Length - 1) * 2 + 4) + "██");
-    Console.Write("██");
-    Console.WriteLine(new string('█', letters.Length * 10 + (letters.Length - 1) * 2 + 6));
+    characterMap.Draw(textMatrixSignatures, letters, isInvertedSign);
   }
-
-
-  /// <summary>
-  /// Converts the input text into an array of uppercase characters.
-  /// Validates that the text is neither null nor empty and does not exceed 12 characters
-  /// </summary>
-  /// <param name="text">The input text to convert.</param>
-  /// <returns>An array of uppercase characters.</returns>
-  /// <exception cref="ArgumentException">Thrown when the text is null, empty, or exceeds 12 characters.</exception>
-  private static char[] GetCharactersArray(string text)
-  {
-    if (string.IsNullOrEmpty(text)) throw new ArgumentException("Text cannot be null or empty.", nameof(text));
-    if (text.Length > 12) throw new ArgumentException("Text length cannot be greater than 12 characters.", nameof(text));
-
-    char[] letters = new char[text.Length];
-
-    // Convert each character to uppercase and store in the array
-    for (int i = 0; i < text.Length; i++)
-    {
-      letters[i] = char.ToUpper(text[i]);
-    }
-    return letters;
-  }
-
-  /// <summary>
-  /// Retrieves the binary signatures for each unique character in the input array.
-  /// </summary>
-  /// <param name="letters">The array of characters to retrieve signatures for.</param>
-  /// <returns>A dictionary mapping each character to its binary signature.</returns>
-  private static Dictionary<char, int[]> GetTextSignatures(char[] letters)
-  {
-    var textSignatures = new Dictionary<char, int[]>();
-
-    // Populate the dictionary with signatures for each unique character
-    for (int i = 0; i < letters.Length; i++)
-    {
-      if (!textSignatures.ContainsKey(letters[i]))
-      {
-        textSignatures.Add(letters[i], CharacterMap.LetterSignatures.First(s => s.Key == letters[i]).Value);
-      }
-    }
-    return textSignatures;
-  }
-
-  /// <summary>
-  /// Constructs a matrix of binary signatures for rendering the text.
-  /// </summary>
-  /// <param name="letters">The array of characters to construct the matrix for.</param>
-  /// <param name="textSignatures">The dictionary containing the binary signatures for each character.</param>
-  /// <returns>A 2D array representing the binary signatures for each character in the text.</returns>
-  private static int[,] GetTextMatrixSignatures(char[] letters, Dictionary<char, int[]> textSignatures)
-  {
-    int[,] textMatrixSignatures = new int[5, letters.Length];
-
-    // Fill the matrix with the signatures for each character
-    for (int line = 0; line < 5; line++)
-    {
-      for (int decimalElement = 0; decimalElement < letters.Length; decimalElement++)
-      {
-        textMatrixSignatures[line, decimalElement] = textSignatures[letters[decimalElement]][line];
-      }
-    }
-    return textMatrixSignatures;
-  }
-
-
-
 }
--- README.md
+++ README.md
@@ -1,7 +1,9 @@
 # ✨ Ascii Sign ✨
 
-[![Static Badge](https://img.shields.io/static/v1?label=AsciiSign&message=NuGet&color=blue&logo=nuget)](https://www.nuget.org/packages/AsciiSign/) ![Downloads](https://img.shields.io/nuget/dt/AsciiSign.svg?logo=nuget) [![NuGet Version](https://img.shields.io/nuget/v/AsciiSign.svg?logo=nuget)](https://www.nuget.org/packages/AsciiSign/) ![.Net](https://img.shields.io/badge/.NET-5C2D91?style=flat&logo=.net&logoColor=white) ![C#](https://img.shields.io/badge/C%23-%23239120.svg?style=flat&logo=csharp&logoColor=white) [![License: MIT](https://img.shields.io/badge/License-MIT-yellow.svg)](https://opensource.org/licenses/MIT)
+[![Static Badge](https://img.shields.io/static/v1?label=AsciiSign&message=NuGet&color=blue&logo=nuget)](https://www.nuget.org/packages/AsciiSign/) ![Downloads](https://img.shields.io/nuget/dt/AsciiSign.svg?logo=nuget) [![NuGet Version](https://img.shields.io/nuget/v/AsciiSign.svg?logo=nuget)](https://www.nuget.org/packages/AsciiSign/) ![.Net](https://img.shields.io/badge/.NET-5C2D91?style=flat&logo=.net&logoColor=white) ![C#](https://img.shields.io/badge/C%23-%23239120.svg?style=flat&logo=csharp&logoColor=white)
+[![License: LGPLv3](https://img.shields.io/badge/License-LGPLv3-red.svg?style=flat)](https://opensource.org/licenses/LGPL-3.0)
 
+**Transform plain text into console art with this lightweight and powerful .NET library.**
 A simple, lightweight, and powerful .NET library for rendering magnificent ASCII art text directly in your console. Turn ordinary words into pixel matrix masterpieces!
 
 ![Rendering Example](https://raw.githubusercontent.com/ericssendelima/ascii-sign/refs/heads/main/assets/normal-sign.png)
@@ -24,90 +26,36 @@ Using `AsciiSign` is incredibly simple and fun.
   dotnet add package AsciiSign
   ```
 
+2.  **Call the drawing methods:**
+    Use the following code snippets in your C# project to see the magic happen:
 2.  Call the `DrawString(string, bool)` method with the string you want to render.
 
-```csharp
-// Import the namespace
-using AsciiSign;
+    ```csharp
 
-Console.Write("Enter text to render in ASCII art:");
-string text = Console.ReadLine();
-Console.Write("Inverted sign (y/n)? ");
-char isInvertedSign = Console.ReadKey().KeyChar.ToString().ToLower()[0];
+    using AsciiSign;
 
-while (isInvertedSign != 'y' && isInvertedSign != 'n')
-{
-  Console.Write("Invalid input. Please enter 'y' or 'n': ");
-  isInvertedSign = Console.ReadKey().KeyChar.ToString().ToLower()[0];
-}
+    // Using the Basic Font
+    Sign.DrawBasicFont("Hello World!", isInvertedSign: false); // Basic Font
 
-// Just one line of code to render the text as ASCII art
-Sign.DrawString(text, isInvertedSign: isInvertedSign == 'y');
-```
+    Console.WriteLine("\n\n"); // Just to add some space
 
-**The result will be something spectacular like this:**
+    // Using the Inverted Basic Font
+    Sign.DrawBasicFont("Hello World!", isInvertedSign: true); // Inverted Basic Font
 
-![Rendering Example](https://raw.githubusercontent.com/ericssendelima/ascii-sign/refs/heads/main/assets/hello-world-example.png)
+    // Using the new and awesome Turing Font!
+    Sign.DrawTuringFont("Hello Turing!");
 
-## 📖 The `AsciiSign` Alphabet
+    Console.WriteLine("\n\n"); // Just to add some space
 
-Each character has a unique "signature," represented by an array of 5 integers. Each integer encodes a row of 5 pixels.
+    // Using the classic Shaded Font
+    Sign.DrawShadedFont("Shaded Style");
 
-<details>
-<summary>Click to see the full character map!</summary>
+    ```
 
-```csharp
-// Letters
-  { 'A', new[] { 14, 17, 31, 17, 17 } },
-  { 'B', new[] { 30, 17, 30, 17, 30 } },
-  { 'C', new[] { 14, 17, 16, 17, 14 } },
-  { 'D', new[] { 30, 17, 17, 17, 30 } },
-  { 'E', new[] { 31, 16, 30, 16, 31 } },
-  { 'F', new[] { 31, 16, 30, 16, 16 } },
-  { 'G', new[] { 14, 16, 23, 17, 14 } },
-  { 'H', new[] { 17, 17, 31, 17, 17 } },
-  { 'I', new[] { 14, 4, 4, 4, 14 } },
-  { 'J', new[] { 7, 2, 2, 18, 12 } },
-  { 'K', new[] { 18, 20, 24, 20, 18 } },
-  { 'L', new[] { 16, 16, 16, 16, 31 } },
-  { 'M', new[] { 17, 27, 21, 17, 17 } },
-  { 'N', new[] { 25, 25, 21, 19, 19 } },
-  { 'O', new[] { 14, 17, 17, 17, 14 } },
-  { 'P', new[] { 30, 17, 30, 16, 16 } },
-  { 'Q', new[] { 14, 17, 17, 18, 15 } },
-  { 'R', new[] { 30, 17, 30, 20, 18 } },
-  { 'S', new[] { 15, 16, 14, 1, 30 } },
-  { 'T', new[] { 31, 4, 4, 4, 4 } },
-  { 'U', new[] { 17, 17, 17, 17, 14 } },
-  { 'V', new[] { 17, 17, 17, 10, 4 } },
-  { 'W', new[] { 17, 21, 21, 21, 10 } },
-  { 'X', new[] { 17, 10, 4, 10, 17 } },
-  { 'Y', new[] { 17, 10, 4, 4, 4 } },
-  { 'Z', new[] { 31, 2, 4, 8, 31 } }
+3.  **Admire the result:**
+    Your console will never be the same!
 
-// Numbers
-  { '0', new[] { 14, 17, 17, 17, 14 } },
-  { '1', new[] { 4, 12, 4, 4, 14 } },
-  { '2', new[] { 30, 1, 30, 16, 31 } },
-  { '3', new[] { 31, 1, 14, 1, 31 } },
-  { '4', new[] { 17, 17, 31, 1, 1 } },
-  { '5', new[] { 31, 16, 31, 1, 31 } },
-  { '6', new[] { 31, 16, 31, 17, 31 } },
-  { '7', new[] { 31, 1, 2, 4, 8 } },
-  { '8', new[] { 31, 17, 31, 17, 31 } },
-  { '9', new[] { 31, 17, 31, 1, 31 } }
-
-// Symbols
-  { ' ', new[] { 0, 0, 0, 0, 0 } },
-  { '.', new[] { 0, 0, 0, 0, 4 } },
-  { '!', new[] { 4, 4, 4, 0, 4 } },
-  { '#', new[] { 10, 31, 10, 31, 10 } },
-  { '+', new[] { 4, 4, 31, 4, 4 } },
-  { '-', new[] { 0, 0, 31, 0, 0 } },
-  { '*', new[] { 21, 14, 31, 14, 21 } },
-  { '/', new[] { 1, 2, 4, 8, 16 } },
-```
-</details>
+    *(This would be a great place for a GIF showing both fonts in action!)*
 
 ## 🤝 How to Contribute
 
@@ -121,8 +69,8 @@ This is a project born from curiosity and a love for creative programming. If yo
 
 ## 📜 License
 
-This project is distributed under the MIT license. See the [LICENSE](./LICENSE) file for more details.
+This project is distributed under the LGPLv3 license. See the [LICENSE](./LICENSE) file for more details.
 
 ---
 
-Created by **Yalê Ericssen** ([@ericssendelima](https://github.com/ericssendelima)).
+ Created with ☕ and a whole lot of code by **Yalê Ericssen** ([@ericssendelima](https://github.com/ericssendelima)).
