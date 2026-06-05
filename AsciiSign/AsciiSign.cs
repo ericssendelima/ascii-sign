6d6d7802a2354e45b044ff56bdbf427204bc16c5
ericssendelima <ericssendelima@gmail.com>
2026-06-05 08:13:31 -0300

    feat: extend library API with new methods

--- AsciiSign/AsciiSign.cs
+++ AsciiSign/AsciiSign.cs
@@ -1,106 +1,99 @@
-﻿/*
- * AsciiSign
- * Copyright (C) 2025 Yalê Ericssen de Lima
- *
- * This program is free software: you can redistribute it and/or modify
- * it under the terms of the GNU Lesser General Public License as published by
- * the Free Software Foundation, either version 3 of the License.
- *
- * This program is distributed in the hope that it will be useful,
- * but WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
- * GNU Lesser General Public License for more details.
- *
- * See more <https://www.gnu.org/licenses/>.
- */
-
-using AsciiSign.utils.characterDictionaries;
+﻿using AsciiSign.interfaces;
 using AsciiSign.utils.enums;
 using AsciiSign.utils.services;
 
 namespace AsciiSign;
 
 /// <summary>
-/// Provides methods to draw ASCII art signs in different font styles.
+/// This class provides methods responsible for creating and drawing ASCII art.
 /// </summary>
-/// <remarks>
-/// Available drawing methods:
-/// - DrawTuringFont: Renders text using the Turing font.
-/// - DrawShadedFont: Renders text using the Shaded font.
-/// - DrawBasicFont: Renders text using the Basic font; accepts an 'isInvertedSign' flag.
-///
-/// Each method processes the input text and uses the corresponding character dictionary
-/// to render the ASCII art to the console.
-/// </remarks>
-
 public static class Sign
 {
   /// <summary>
-  /// Draws text using the Turing font. 
+  /// Draws the ASCII art on the console based on the provided text and whether to use inverted signs or not.
   /// </summary>
-  /// <param name="text">
-  /// The text to be rendered using the Turing font.
-  /// </param>
-  /// <remarks>
-  /// This method processes the input text and uses the Turing character dictionary
-  /// to render the ASCII art to the console.
-  /// </remarks>
-  /// <example>
-  /// Sign.DrawTuringFont("Hello World");
-  /// </example>
-  public static void DrawTuringFont(string text)
-  {
-    DrawString<string>(text, FontType.TURING, Turing.Instance, MatrixHeight.SIX);
-  }
-  /// <summary>
-  /// Draws text using the Shaded font. 
-  /// </summary>
-  /// <param name="text">
-  /// The text to be rendered using the Shaded font.
-  /// </param>
-  /// <remarks>
-  /// This method processes the input text and uses the Shaded character dictionary
-  /// to render the ASCII art to the console.
-  /// </remarks>
-  /// <example>
-  /// Sign.DrawShadedFont("Hello World");
-  /// </example>
-  public static void DrawShadedFont(string text)
-  {
-    DrawString<string>(text, FontType.SHADED, Shaded.Instance, MatrixHeight.SIX);
-  }
-  /// <summary>
-  /// Draws text using the Basic font.
-  /// </summary>
-  /// <param name="text">
-  /// The text to be rendered using the Basic font.
-  /// </param>
-  /// <param name="isInvertedSign">
-  /// A flag indicating whether the sign should be inverted.
-  /// </param>
-  /// <remarks>
-  /// This method processes the input text and uses the Basic character dictionary
-  /// to render the ASCII art to the console. The 'isInvertedSign' flag determines
-  /// whether the sign is inverted.
-  /// </remarks>
-  /// <example>
-  /// Sign.DrawBasicFont("Hello World", true);
-  /// Sign.DrawBasicFont("Hello World", false);
-  /// </example>
+  /// <param name="text">The text for which to draw ASCII art.</param>
+  /// <param name="isInvertedSign">A value indicating whether to use inverted signs.</param>
+  [Obsolete("This method is obsolete and will be removed in the upcoming version 3.0. " +
+    "Please use the new methods DrawOnConsole(\"abcd\", FontType.BASIC), DrawOnConsole(\"abcd\", FontType.INVERTED_BASIC)" +
+    " or var someMatrix = GetAsciiArtMatrix(\"abcd\", FontType.BASIC), var someMatrix = GetAsciiArtMatrix(\"abcd\", FontType.INVERTED_BASIC).")]
   public static void DrawBasicFont(string text, bool isInvertedSign)
   {
-    DrawString<int>(text, FontType.BASIC, Basic.Instance, MatrixHeight.FIVE, isInvertedSign);
+    DrawOnConsole(text, isInvertedSign ? FontType.INVERTED_BASIC : FontType.BASIC);
   }
 
-  private static void DrawString<T>(string text, FontType fontType, CharacterDictionary<T> characterMap, MatrixHeight matrixHeight, bool? isInvertedSign = false)
+  /// <summary>
+  /// Draws the ASCII art on the console based on the provided text using the Turing font.
+  /// </summary>
+  /// <param name="text">The text for which to draw ASCII art.</param>
+  [Obsolete("This method is obsolete and will be removed in the upcoming version 3.0. " +
+    "Please use the new methods DrawOnConsole(\"abcd\", FontType.TURING) " +
+    "or var matrix = GetAsciiArtMatrix(\"abcd\", FontType.TURING)")]
+  public static void DrawTuringFont(string text)
   {
-    // Get the array of characters from the input text
-    char[] letters = Characters.GetArray(text, fontType);
+    DrawOnConsole(text, FontType.TURING);
+  }
 
-    // Get the matrix signatures for the text
-    T[,] textMatrixSignatures = DataProcessing<T>.GetTextMatrixSignatures(text, fontType, characterMap, matrixHeight, letters);
+  /// <summary>
+  /// Draws the ASCII art on the console based on the provided text using the shaded font.
+  /// </summary>
+  /// <param name="text">The text for which to draw ASCII art.</param>
+  [Obsolete("This method is obsolete and will be removed in the upcoming version 3.0. " +
+    "Please use the new methods DrawOnConsole(\"abcd\", FontType.SHADED)" +
+    " or var matrix = GetAsciiArtMatrix(\"abcd\", FontType.SHADED).")]
+  public static void DrawShadedFont(string text)
+  {
+    DrawOnConsole(text, FontType.SHADED);
+  }
 
-    // Draw the text using the character map
-    characterMap.Draw(textMatrixSignatures, letters, isInvertedSign);
+  /// <summary>
+  /// Draws the ASCII art on the console based on the provided text and font type.
+  /// </summary>
+  /// <param name="text">The text for which to draw ASCII art.</param>
+  /// <param name="fontType">The type of font to use.</param>
+  public static void DrawOnConsole(string text, FontType fontType)
+  {
+    MatrixHeight matrixHeight = TextMatrix.GetMatrixHeight(fontType);
+
+    string[,] matrixTranslated = GetAsciiArtMatrix(text, fontType, consoleDrawing: true);
+
+    DrawMatrix(matrixTranslated, (int)matrixHeight);
+  }
+
+
+  /// <summary>
+  /// Generates a matrix of strings representing the ASCII art for the given text and font type.
+  /// </summary>
+  /// <param name="text">The text for which to generate ASCII art.</param>
+  /// <param name="fontType">The type of font to use.</param>
+  /// <returns>A matrix of strings representing the ASCII art.</returns>
+  public static string[,] GetAsciiArtMatrix(string text, FontType fontType)
+    => GetAsciiArtMatrix(text, fontType, consoleDrawing: false);
+  private static string[,] GetAsciiArtMatrix(string text, FontType fontType, bool consoleDrawing)
+  {
+    ICharacterDictionary characterDictionary = Characters.GetCharacterDictionary(fontType);
+    MatrixHeight matrixHeight = TextMatrix.GetMatrixHeight(fontType);
+
+    string[,] matrixTranslated = DataProcessing.FinalMatrixProcessment(text, fontType, characterDictionary, matrixHeight, consoleDrawing);
+
+    return matrixTranslated;
+  }
+
+  /// <summary>
+  /// Draws the ASCII art represented by the provided matrix on the console.
+  /// </summary>
+  /// <param name="textToDraw">The matrix of strings representing the ASCII art.</param>
+  /// <param name="matrixHeight">The height of the matrix.</param>
+  private static void DrawMatrix(string[,] textToDraw, int matrixHeight)
+  {
+    for (int i = 0; i < matrixHeight; i++)
+    {
+      for (int decimalElement = 0; decimalElement < textToDraw.GetLength(1); decimalElement++)
+      {
+        Console.Write(textToDraw[i, decimalElement]);
+      }
+      Console.WriteLine();
+    }
+    Console.WriteLine();
   }
 }
---
+++ AsciiSign/utils/extensions/ColumnControlExtensions.cs
@@ -0,0 +1,83 @@
+﻿using AsciiSign.interfaces;
+using AsciiSign.utils.enums;
+using AsciiSign.utils.services;
+
+namespace AsciiSign.utils.extensions
+{
+  /// <summary>
+  /// This class provides extension methods for managing the character dictionary and converting text matrix signatures into an ASCII art matrix.
+  /// It includes logic to determine the starting column for each letter based on the character length and matrix height, ensuring proper
+  /// alignment of the ASCII art representation.
+  /// </summary>
+  internal static class ColumnControlExtensions
+  {
+    /// <summary>
+    /// Converts the text matrix signatures into an ASCII art matrix based on the provided character dictionary, letters, and matrix height.
+    /// </summary>
+    /// <param name="characterDictionary">The character dictionary containing the ASCII art representations.</param>
+    /// <param name="textMatrixSignatures">The text matrix signatures to convert.</param>
+    /// <param name="letters">The array of letters to render.</param>
+    /// <param name="matrixHeight">The height of the ASCII art matrix.</param>
+    /// <returns>The resulting ASCII art matrix.</returns>
+    internal static string[,] ToAsciiArtMatrix(this ICharacterDictionary characterDictionary, string[,] textMatrixSignatures, 
+      char[] letters, MatrixHeight matrixHeight)
+    {
+      int matrixHeightInteger = (int)matrixHeight;
+
+      var characterLength = (textMatrixSignatures[0,0].Length * letters.Length);
+      var response = new string[matrixHeightInteger, characterLength];
+
+      for (int line = 0; line < matrixHeightInteger; line++) // Number of lines in the ASCII art matrix
+      {
+        for (int column = 0; column < letters.Length; column++) // Number of letters to render
+        {
+          int currentLetterColumnPosition = GetLetterStartColumn(textMatrixSignatures, matrixHeightInteger, column);
+
+          // Get the hexadecimal string representation for the current line and letter
+          string hex = textMatrixSignatures[line, column];
+          for (int hexColumn = 0; hexColumn < hex.Length; hexColumn++) 
+          {
+            response[line, currentLetterColumnPosition] = Characters.GetRelactiveAsciiValue(hex[hexColumn]);
+            currentLetterColumnPosition++;
+          }
+        }
+      }
+      return response;
+    }
+
+    /// <summary>
+    /// Determines the starting column for a letter in the ASCII art matrix based on the character length and matrix height.
+    /// This relationship changes according to the character length and matrix height.
+    /// </summary>
+    /// <param name="textMatrixSignatures">The text matrix signatures.</param>
+    /// <param name="matrixHeight">The height of the ASCII art matrix.</param>
+    /// <param name="column">The column index of the letter.</param>
+    /// <returns>The starting column for the letter.</returns>
+    private static int GetLetterStartColumn(string[,] textMatrixSignatures, int matrixHeight, int column)
+    {
+      int response;
+      int characterLength = textMatrixSignatures[0, 0].Length;
+
+      if (characterLength == matrixHeight)
+      {
+        response = matrixHeight * column;
+      }
+      else if (characterLength < matrixHeight)
+      {
+        if (matrixHeight - characterLength > 2)
+        {
+          response = (matrixHeight - 3) * column;
+        }
+        else
+        {
+          response = (matrixHeight - 2) * column;
+        }
+      }
+      else
+      {
+        response = (matrixHeight + 1) * column;
+      }
+      return response;
+    }
+  }
+}
