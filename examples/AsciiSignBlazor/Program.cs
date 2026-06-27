ab36b64b11c645b9980d7b9c0e47c28db1be50f1
ericssendelima <ericssendelima@gmail.com>
2026-06-27 15:49:14 -0300

    fix(examples): Disabled app.UseHttpsRedirection() to allow smooth SSL termination at the edge. Prevents internal redirect conflicts that break SignalR/WebSocket connections.

--- examples/AsciiSignBlazor/Program.cs
+++ examples/AsciiSignBlazor/Program.cs
@@ -25,7 +25,7 @@ if (!app.Environment.IsDevelopment())
 }
 
 app.UseStatusCodePagesWithReExecute("/not-found", createScopeForStatusCodePages: true);
-app.UseHttpsRedirection();
+//app.UseHttpsRedirection();
 app.UseAntiforgery();
 app.MapStaticAssets();
 
